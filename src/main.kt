d5f3003737bd8a4cc6420967ab44f85c0e8a93cc
aaaaaa2493 <aaaaaa2493@yandex.ru>
2018-08-22 16:03:24 +0300

    Stage 1 completed

---
+++ src/main.kt
@@ -0,0 +1,12 @@
+
+fun main(args: Array<String>) {
+
+    println("Starting to create a coffee")
+    println("Grinding coffee beans")
+    println("Boiling water")
+    println("Mixing boiled water with crushed coffee beans")
+    println("Pouring coffee into the cup")
+    println("Pouring some milk into the cup")
+    println("Coffee is ready!")
+
+}
