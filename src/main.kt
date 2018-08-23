f9677eaa88e3c99ab3853e7d2944572abf905912
aaaaaa2493 <aaaaaa2493@yandex.ru>
2018-08-23 17:17:59 +0300

    Stage 4 completed

--- src/main.kt
+++ src/main.kt
@@ -1,32 +1,106 @@
 import java.util.*
 
-fun main(args: Array<String>) {
+var waterAmount = 1200
+var milkAmount = 540
+var coffeeBeansAmount = 120
+var disposableCupsAmount = 9
+var moneyAmount = 550
 
+fun printSupplies() {
+    println("The coffee machine has:")
+    println("$waterAmount of water")
+    println("$milkAmount of milk")
+    println("$coffeeBeansAmount of coffee beans")
+    println("$disposableCupsAmount of disposable cups")
+    println("$moneyAmount of money")
+}
+
+fun processBuy() {
     val scanner = Scanner(System.`in`)
 
-    print("Write how many ml of water the coffee machine has: ")
+    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
+    val answer = scanner.nextInt()
+
+    val requiredWater: Int
+    val requiredMilk: Int
+    val requiredCoffeeBeans: Int
+    val requiredMoney: Int
+
+    when (answer) {
+        1 -> {
+            requiredWater = 250
+            requiredMilk = 0
+            requiredCoffeeBeans = 16
+            requiredMoney = 4
+        }
+        2 -> {
+            requiredWater = 350
+            requiredMilk = 75
+            requiredCoffeeBeans = 20
+            requiredMoney = 7
+        }
+        3 -> {
+            requiredWater = 200
+            requiredMilk = 100
+            requiredCoffeeBeans = 12
+            requiredMoney = 6
+        }
+        else -> {
+            println("Bad command!")
+            return
+        }
+    }
+
+    waterAmount -= requiredWater
+    milkAmount -= requiredMilk
+    coffeeBeansAmount -= requiredCoffeeBeans
+    moneyAmount -= requiredMoney
+    disposableCupsAmount -= 1
+}
+
+fun processFill() {
+    val scanner = Scanner(System.`in`)
+
+    print("Write how many ml of water do you want to add: ")
     val mlWater = scanner.nextInt()
 
-    print("Write how many ml of milk the coffee machine has: ")
+    print("Write how many ml of milk do you want to add: ")
     val mlMilk = scanner.nextInt()
 
-    print("Write how many grams of coffee beans the coffee machine has: ")
+    print("Write how many grams of coffee beans do you want to add: ")
     val gramBeans = scanner.nextInt()
 
-    print("Write how many cups of coffee you will need: ")
+    print("Write how many disposable cups of coffee do you want to add: ")
     val cupsAmount = scanner.nextInt()
 
-    val canMake = minOf(mlWater / 200, mlMilk / 50, gramBeans / 15)
+    waterAmount += mlWater
+    milkAmount += mlMilk
+    coffeeBeansAmount += gramBeans
+    disposableCupsAmount += cupsAmount
+}
 
-    if (canMake == cupsAmount) {
-        println("Yes, I can make such amount of coffee")
+fun processTake() {
+    println("I gave you $moneyAmount$")
+    moneyAmount = 0
+}
+
+fun main(args: Array<String>) {
+
+    printSupplies()
+
+    val scanner = Scanner(System.`in`)
+    print("Write action (buy, fill, take): ")
+    val answer = scanner.nextLine()
+
+    when (answer){
+        "buy" -> processBuy()
+        "fill" -> processFill()
+        "take" -> processTake()
+        else -> {
+            println("Bad action!")
+        }
     }
-    else if (canMake > cupsAmount) {
-        val additionalCups = canMake - cupsAmount
-        println("Yes, I can make such amount of coffee (and even $additionalCups more than that)")
-    }
-    else {
-        println("No, I can make only $canMake cups of coffee")
-    }
+
+    printSupplies()
 
 }
